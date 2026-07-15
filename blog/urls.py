f1b6da8797e8d0461d4409a74aef94f88472bc53
natnael-natnael <natnaelabebegualu@gmail.com>
2026-07-15 21:11:29 +0300

    blog list and blog detail page complited

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from . import views
 # http://127.0.0.1:8000/blogs/
-# http://127.0.0.1:8000/blogs/delete/
+# http://127.0.0.1:8000/blogs/5/
 urlpatterns = [
-    path('', views.index, name='blog_index'),
-    path('delete/', views.delete, name='blog_delete'),
+    path('', views.blog_list, name='blog_list'),
+    path('<int:pk>/', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello, world.</h1><p>This is the ethix application.</p>")
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-def delete(request):
-    return HttpResponse("<h1>Delete this page</h1>")
+# Create your views here.
+def blog_list(request):
+    blogs = Blog.objects.filter(isPublished=True).order_by('-created_at')
+    return render(request, 'blog/list.html', {'blogs': blogs})
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk, isPublished=True)
+    return render(request, 'blog/detail.html', {'blog': blog})
